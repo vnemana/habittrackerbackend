3820af14aa646dd0bc72677ec0ef1fe9487b14de
venkat <venkat.nemana@gmail.com>
2023-09-04 19:40:35 -0700

    minor refactor

--- tests/db/habits.test.js
+++ tests/db/habits.test.js
@@ -38,6 +38,21 @@ function createHabit(name, userId, frequency, timeOfDay, active) {
     };
 }
 
+async function addHabit() {
+    //First add the user
+    let user = createUser(1, "venkat.nemana.dev@test.com", "Venkat", "Nemana", "Venkat Nemana");
+    await UsersObj.add(user);
+
+    //Now add the Habit
+    let timeOfDayDate = new Date(2023, 3, 1, 12, 30, 30);
+    let timeOfDay = timeOfDayDate.getHours() + ":" + timeOfDayDate.getMinutes() + ":" + timeOfDayDate.getSeconds();
+    let habit = createHabit('Walking', '1', 'Daily',
+        timeOfDay, true);
+    await HabitsObj.add(habit);
+    let result = await HabitsObj.getByUserId('1');
+    return {timeOfDay, result};
+}
+
 describe ("test add and get", () => {
     beforeEach(async() => {
         await HabitsObj.deleteByUserId('1');
@@ -48,37 +63,17 @@ describe ("test add and get", () => {
         await UsersObj.deleteByUserId('1');
     });
     test('add with proper input works', async() => {
-
-        //First add the user
-        let user = createUser(1, "venkat.nemana.dev@test.com", "Venkat", "Nemana", "Venkat Nemana");
-        await UsersObj.add(user);
-
-        //Now add the Habit
-        let timeOfDayDate = new Date(2023, 3, 1, 12, 30, 30);
-        let timeOfDay = timeOfDayDate.getHours() + ":" + timeOfDayDate.getMinutes() + ":" + timeOfDayDate.getSeconds();
-        let habit = createHabit('Walking', '1', 'Daily',
-            timeOfDay, true);
-        await HabitsObj.add(habit);
-        let result = await HabitsObj.getByUserId('1');
+        let {timeOfDay, result} = await addHabit();
         expect(result.length).toBe(1);
         assertHabitContent(result, 'Walking', '1', 'Daily', timeOfDay,
             true);
     });
 
     test('update habit name', async() => {
-        //First add the user
-        let user = createUser(1, "venkat.nemana.dev@test.com", "Venkat", "Nemana", "Venkat Nemana");
-        await UsersObj.add(user);
-
-        //Now add the Habit
-        let timeOfDayDate = new Date(2023, 3, 1, 12, 30, 30);
-        let timeOfDay = timeOfDayDate.getHours() + ":" + timeOfDayDate.getMinutes() + ":" + timeOfDayDate.getSeconds();
-        let habit = createHabit('Walking', '1', 'Daily',
-            timeOfDay, true);
-        await HabitsObj.add(habit);
-        habit = createHabit('Walking', '1', 'Weekly', timeOfDay, true);
-        let result = await HabitsObj.getByUserId('1');
+        let {timeOfDay, result} = await addHabit();
         expect(result.length).toBe(1);
+
+        habit = createHabit('Walking', '1', 'Weekly', timeOfDay, true);
         await HabitsObj.update(habit, result[0].id);
         result = await HabitsObj.getByUserId('1');
         expect(result.length).toBe(1);
